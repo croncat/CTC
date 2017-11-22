ecdb100968642a098c7db0895a09a6588eb75a50
k0ch <k0ch@riseup.net>
2017-11-22 14:13:39 -0300

    compile-time factorial

---
+++ 1-factorial/factorial.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+template<int n> 
+struct factorial 
+{
+    enum { ret = factorial<n-1>::ret * n }; 
+};
+
+template<> 
+struct factorial<0> 
+{
+    enum { ret = 1 }; 
+};
+
+int main()
+{
+    //compile-time '6!'
+    std::cout << factorial<6>::ret << std::endl;
+}
